2cf7606ad85774b6a3e91522aa11632e72a46e34
asus <trias.2001@students.amikom.ac.id>
2020-12-16 09:33:58 +0700

    Add project files.

---
+++ OrderFood/Controller/MainWindoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OrderFood.MODEL;
+
+namespace OrderFood.Controller
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+    }
+}
